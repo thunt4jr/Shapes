9cac41169a75ba3f77afef78d164bb87818e0796
thunt <thunt@DESKTOP-VU8GQLN>
2021-03-03 17:58:36 -0500

    Add project files.

---
+++ Shapes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class FindShapes
+    {
+
+    }
+    class Point
+    {
+
+    }
+    class Triangle
+    {
+
+    }
+
+    class Circle
+    {
+
+    }
+
+    class Rectangle
+    {
+
+    }
+
+}
