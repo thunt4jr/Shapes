0ec5a00c825081b5e26b28ad458f6745aef3595a
thunt <thunt@DESKTOP-VU8GQLN>
2021-03-03 18:15:34 -0500

    adding test project

---
+++ Shapes.Test/Shapes.Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit;
+
+namespace Shapes.Test
+{
+    public class ShapesTest
+    {
+
+        static void Main()
+        {
+
+        }
+
+    }
+
+
+}
